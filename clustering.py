98a363c1f1fffbd6a28a0f29e46b21c8ba7d997d
Litrid <65836019+Litrid@users.noreply.github.com>
2024-04-01 01:12:40 -0400

    Reformatted clustering.py, fixed PyTA errors

--- clustering.py
+++ clustering.py
@@ -1,19 +1,21 @@
 """TODO: EMPTY DOCSTRING"""
 
 import movie_class
-import load_graph
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def Σ_in(community: list[set[movie_class.Movie] | float]):
+
+def sigma_in(community: list[set[movie_class.Movie] | float]):
     """TODO: Docstring for """
     return community[1]
 
-def Σ_tot(community: list[set[movie_class.Movie] | float]) -> float:
+
+def sigma_tot(community: list[set[movie_class.Movie] | float]) -> float:
     incident_edges = 0
     for vertex in community[0]:
         incident_edges += vertex.sum_weights
-    return incident_edges / 2 # TODO divide by 2 bc overcounting?
+    return incident_edges / 2  # TODO divide by 2 bc overcounting?
+
 
 def k_i_in(added_vertex: movie_class.Movie, community: list[set[movie_class.Movie] | float]) -> float:
     """TODO: Docstring for k_i_in"""
@@ -39,6 +41,7 @@ def k_i(added_vertex: movie_class.Movie):
     """TODO: Docstring for k_i"""
     return added_vertex.sum_weights
 
+
 def m_func(graph: movie_class.Network):
     """TODO: Docstring for m"""
     all_edge_weight = 0
@@ -46,30 +49,32 @@ def m_func(graph: movie_class.Network):
         all_edge_weight += graph.get_movies()[vertex].sum_weights
     return all_edge_weight / 2
 
+
 def louvain(graph: movie_class.Network, epochs: int) -> None:
     """TODO: Docstring for louvain"""
     m = m_func(graph)
     for _ in range(epochs):
         for vertex_name in graph.get_movies():
             vertex = graph.get_movies()[vertex_name]
-            max_Q = 0
+            max_q = 0
             best_community = vertex.community
             for neighbour in vertex.neighbours:
                 print(vertex_name, neighbour.title)
                 community = graph.get_communities()[neighbour.community]
-                sum_in = Σ_in(community)
-                sum_tot = Σ_tot(community)
+                sum_in = sigma_in(community)
+                sum_tot = sigma_tot(community)
                 ki = k_i(vertex)
                 kin = k_i_in(vertex, community)
                 # print(sum_in, sum_tot)
 
-                deltaQ = (((sum_in + kin) / (2 * m)) - (((sum_tot + ki) / (2 * m)) ** 2)) - ((sum_in / (2 * m)) - ((sum_tot / (2 * m)) ** 2) - (ki / (2 * m)) ** 2)
-                # print(deltaQ)
-                if max_Q < deltaQ:
-                    max_Q = deltaQ
+                delta_q = (((sum_in + kin) / (2 * m)) - (((sum_tot + ki) / (2 * m)) ** 2)) - (
+                            (sum_in / (2 * m)) - ((sum_tot / (2 * m)) ** 2) - (ki / (2 * m)) ** 2)
+                # print(delta_q)
+                if max_q < delta_q:
+                    max_q = delta_q
                     best_community = neighbour.community
 
-            if max_Q > 0:
+            if max_q > 0:
                 neighbour_community = graph.get_communities()[best_community]
                 old_community = graph.get_communities()[vertex.community]
                 best_k_i_in = k_i_in(vertex, neighbour_community)
@@ -140,11 +145,8 @@ for u in g._communities:
         ans = ''
         for v in g._communities[u][0]:
             ans += v.title + ', '
-        print(len(g._communities[u][0]), 'title: ', u,' ', ans)
+        print(len(g._communities[u][0]), 'title: ', u, ' ', ans)
         # print(len(g._communities[u][0]), g._communities[u][0])
-
-
-
 
 # def community_density(edge: float, vertices: int) -> float:
 #     """Return the density of the community"""
@@ -267,7 +269,6 @@ for u in g._communities:
 #         #     ans += v.title + ', '
 #         print(len(g._communities[u][0]))
 #         # print(len(g._communities[u][0]), g._communities[u][0])
-
 
 
 # # TEST GRAPH
