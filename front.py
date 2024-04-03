982bdcf930a7e2d05ca77218e92bc81aff1aafdb
FionaV2400 <139073560+FionaVerzivolli@users.noreply.github.com>
2024-04-02 23:57:12 -0400

    take functions out of main

--- front.py
+++ front.py
@@ -6,7 +6,7 @@ from tkinter import Label, Entry, Listbox, END, messagebox
 
 """
 TODO:
-- Fix search: DONE
+- Fix search: in progress
 - Fix movietitle: DONE
 - Fix select bug: in progress
     each time you select a movie, it automatically selects the first movie in our entry box
@@ -14,45 +14,22 @@ TODO:
 - Add comments: in progress
 """
 
-root = tk.Tk()
-
-list_of_movies = []
-
-for line in csv.reader(open('movies.csv', 'r')):
-    list_of_movies += [line[2]]
-
-item = list_of_movies.pop(0)  # remove first MovieTitle
-
-root.geometry("2560x1600")
-root.title("Project 2 Window")
-label = tk.Label(root, text="Movie Recommender", font=('Arial', 80))
-label.pack(padx=20, pady=20)
-label2 = tk.Label(root, text="To begin, enter a movie you have watched or like, \nand your desired number of reviews",
-                  font=('Arial', 25))
-label2.pack(padx=20, pady=10)
-
-movie_input1 = Label(root, text="Selected Movies", font=('Arial', 20))
-movie_input1.pack()
-
-selected_movies_listbox = tk.Listbox(root, width=50, height=5)
-selected_movies_listbox.pack(pady=5)
-
-movie_input2 = Label(root, text="Enter Movie(s) By Clicking or Typing", font=('Arial', 20))
-movie_input2.pack(pady=5)
-
-movie_entry = Entry(root, font=('Arial', 20))
-movie_entry.pack(pady=5)
-
-movies = Listbox(root, width=50, height=5)
-movies.pack(pady=5)
-
-movie_input3 = Label(root, text="Maximum number of reviews", font=('Arial', 20))
-movie_input3.pack(pady=5)
-
-spinbox = tk.Spinbox(root, from_=0, to=50)
-spinbox.pack(padx=20, pady=5)
-
 selected_movies = set()  # store the movies we are working with
+
+
+def updater(event):
+    """Replace entry content with selected movie and store it"""
+    if len(selected_movies) >= 5:
+        # error message
+        tk.messagebox.showinfo("Limit Reached",
+                               "You can only select up to 5 movies. Press recommend and try again.")
+        return
+
+    movie_name = movies.get(tk.ACTIVE)
+    movie_entry.delete(0, tk.END)
+    movie_entry.insert(tk.END, movie_name)
+    selected_movies.add(movie_name)
+    update_selected_movies()
 
 
 def modify(lst) -> Any:
@@ -62,18 +39,13 @@ def modify(lst) -> Any:
         movies.insert(END, movie)
 
 
-def updater(event):
-    """Replace entry content with selected movie and store it"""
-    if len(selected_movies) >= 5:
-        # error message
-        tk.messagebox.showinfo("Limit Reached", "You can only select up to 5 movies. Press recommend and try again.")
-        return
-
-    movie_name = movies.get(tk.ACTIVE)
-    movie_entry.delete(0, tk.END)
-    movie_entry.insert(tk.END, movie_name)
-    selected_movies.add(movie_name)
-    update_selected_movies()
+def generate_movies(movies_file: str) -> list:
+    """Function needed to store movies in a list"""
+    movie_lst = []
+    for line in csv.reader(open(movies_file, 'r')):
+        movie_lst += [line[2]]
+    item = movie_lst.pop(0)  # remove first MovieTitle
+    return movie_lst
 
 
 def verify(event) -> Any:
@@ -105,9 +77,6 @@ def update_selected_movies(event=None):
         selected_movies_listbox.insert(tk.END, movie)
 
 
-selected_movies_listbox.bind("<<ListboxSelect>>", update_selected_movies)
-
-
 def add_movie():
     """TODO: Complete docstring"""
     global selected_movies
@@ -115,13 +84,49 @@ def add_movie():
     update_selected_movies()
 
 
-button = tk.Button(root, text="Recommend", font=('Arial', 30), command=recommend_movies)
-button.pack()
+if __name__ == '__main__':
+    root = tk.Tk()
 
-modify(list_of_movies)
+    list_of_movies = generate_movies('movies.csv')
 
-movies.bind("<<ListboxSelect>>", updater)
-movie_entry.bind("<KeyRelease>", verify)
+    root.geometry("2560x1600")
+    root.title("Project 2 Window")
+    label = tk.Label(root, text="Movie Recommender", font=('Arial', 80))
+    label.pack(padx=20, pady=20)
+    label2 = tk.Label(root,
+                      text="To begin, enter a movie you have watched or like, \nand your desired number of reviews",
+                      font=('Arial', 25))
+    label2.pack(padx=20, pady=10)
 
+    movie_input1 = Label(root, text="Selected Movies", font=('Arial', 20))
+    movie_input1.pack()
 
-root.mainloop()
+    selected_movies_listbox = tk.Listbox(root, width=50, height=5)
+    selected_movies_listbox.pack(pady=5)
+
+    movie_input2 = Label(root, text="Enter movie(s) by typing, select movie, and hit enter.", font=('Arial', 20))
+    movie_input2.pack(pady=5)
+
+    movie_entry = Entry(root, font=('Arial', 20))
+    movie_entry.pack(pady=5)
+
+    movies = Listbox(root, width=50, height=5)
+    movies.pack(pady=5)
+
+    movie_input3 = Label(root, text="Maximum number of recommendations", font=('Arial', 20))
+    movie_input3.pack(pady=5)
+
+    spinbox = tk.Spinbox(root, from_=0, to=5)
+    spinbox.pack(padx=20, pady=5)
+
+    selected_movies_listbox.bind("<<ListboxSelect>>", update_selected_movies)
+
+    button = tk.Button(root, text="Recommend", font=('Arial', 30), command=recommend_movies)
+    button.pack()
+
+    modify(list_of_movies)
+
+    movies.bind("<<ListboxSelect>>", updater)
+    movie_entry.bind("<ButtonRelease>", verify)
+
+    root.mainloop()
